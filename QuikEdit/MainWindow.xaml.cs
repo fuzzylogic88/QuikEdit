8412d2e79af1f63aa849ddb239d24187fa0e5cea
Daniel Green <daniel.green927@outlook.com>
2023-12-23 02:04:16 -0800

    Cleanup

--- QuikEdit/MainWindow.xaml.cs
+++ QuikEdit/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using ImageMagick;
 using Microsoft.Win32;
+using System.DirectoryServices.ActiveDirectory;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -38,12 +39,13 @@ namespace QuikEdit
                 loadedBitmapImage = new BitmapImage(new Uri(openFileDialog.FileName));
                 imageControl.Source = loadedBitmapImage;
 
-                using (MemoryStream stream = new MemoryStream())
+                using (MemoryStream stream = new())
                 {
                     BitmapEncoder encoder = new JpegBitmapEncoder(); // Change the encoder based on your image format
                     encoder.Frames.Add(BitmapFrame.Create(loadedBitmapImage));
                     encoder.Save(stream);
                     initialState = stream.ToArray();
+                    modifiedImage = initialState;
                 }
             }
         }
@@ -57,7 +59,7 @@ namespace QuikEdit
                 bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
                 bitmapImage.StreamSource = stream;
                 bitmapImage.EndInit();
-                bitmapImage.Freeze(); 
+                bitmapImage.Freeze();
             }
             return bitmapImage;
         }
@@ -78,9 +80,9 @@ namespace QuikEdit
                             imageControl.Source = ByteArrayToBitmapImage(image.ToByteArray());
                         }
                     }
-                    else 
-                    { 
-                        imageControl.Source = ByteArrayToBitmapImage(initialState); 
+                    else
+                    {
+                        imageControl.Source = ByteArrayToBitmapImage(initialState);
                     }
                 }
             }
@@ -93,41 +95,36 @@ namespace QuikEdit
 
         private async void SaveFileButton_Click(object sender, RoutedEventArgs e)
         {
-            SaveFileDialog saveFileDialog = new()
+            if (modifiedImage != null)
             {
-                Filter = "PNG Files (*.png)|*.png|JPEG Files (*.jpg)|*.jpg|All files (*.*)|*.*"
-            };
-            if (saveFileDialog.ShowDialog() == true)
-            {
-                if (modifiedImage == null) 
-                { 
-                    modifiedImage = initialState; 
-                }
-
-                SaveImage(modifiedImage, saveFileDialog.FileName, GetImageFormatFromFileName(saveFileDialog.FileName));
-            }
-        }
-
-        private void SaveImage(byte[] imagedata, string filename, ImageFormat format)
-        {
-            if (imagedata != null && imagedata.Length > 0)
-            {
-                // Save the byte array to a file with the specified image format
-                using (MemoryStream stream = new(imagedata))
+                SaveFileDialog saveFileDialog = new()
                 {
-                    using Bitmap bitmap = new(stream);
-                    bitmap.Save(filename, format);
+                    Filter = "BMP Files (*.bmp)|*.bmp|GIF Files (*.gif)|*.gif|JPEG Files (*.jpg;*.jpeg)|*.jpg;*.jpeg|PNG Files (*.png)|*.png|All files (*.*)|*.*"
+                };
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    if (modifiedImage.Length > 0)
+                    {
+                        // Save the byte array to a file with the specified image format
+                        using (MemoryStream stream = new(modifiedImage))
+                        {
+                            using Bitmap bitmap = new(stream);
+                            bitmap.Save(saveFileDialog.FileName, GetImageFormatFromFileName(saveFileDialog.FileName));
+                        }
+                        MessageBox.Show("Image saved successfully.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Image data is null or empty. Unable to save.");
+                    }
                 }
-                MessageBox.Show("Image saved successfully.");
-            }
-            else
-            {
-                MessageBox.Show("Image data is null or empty. Unable to save.");
             }
         }
+
         private ImageFormat GetImageFormatFromFileName(string fileName)
         {
-            string extension = System.IO.Path.GetExtension(fileName).ToLowerInvariant();
+            string extension = Path.GetExtension(fileName).ToLowerInvariant();
             switch (extension)
             {
                 case ".bmp":
